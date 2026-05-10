9929ba692a1be23bfea757179b0d31d21aba2e81
kanon8888 <mdrakibhasankanon@gmail.com>
2026-05-11 00:55:04 +0600

    three commit

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,4 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../service/product.service";
 
 
 export const productController = (req: IncomingMessage, res: ServerResponse) => {
@@ -6,13 +7,14 @@ export const productController = (req: IncomingMessage, res: ServerResponse) => 
     const method = req.method
     if (url === "/products" && method === "GET") {
 
-        const product = [
-            {
-                i: 1,
-                name: "Product - 1",
-            },
+        // const product = [
+        //     {
+        //         i: 1,
+        //         name: "Product - 1",
+        //     },
 
-        ];
+        // ];
+        const product = readProduct();
 
         res.writeHead(200, { "content-type": "application/json" });
         res.end(JSON.stringify({ message: "Products retrived succeefully", data: product }));
