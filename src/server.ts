d5db618b738eb5bb73cfc29d1529911735e4147d
kanon8888 <mdrakibhasankanon@gmail.com>
2026-05-12 23:23:33 +0600

    fivteen commit

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { createServer, IncomingMessage, Server } from "http";
 import { url } from "inspector";
 import { routeHandler } from "./routes/route";
+import config from "./config";
 
 const server: Server = createServer((req: IncomingMessage, res) => {
     routeHandler(req, res);
@@ -9,6 +10,6 @@ const server: Server = createServer((req: IncomingMessage, res) => {
 },
 );
 
-server.listen(5000, () => {
-    console.log("Server is running on the port 5000")
+server.listen(config.port, () => {
+    console.log("Server is running on the port 80000")
 });
